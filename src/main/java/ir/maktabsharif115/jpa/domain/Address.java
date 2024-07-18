cc256cdfa9a98247c398cec76b7bc50e88e5fb2e
maxixking66 <maxixking66@gamil.com>
2024-07-18 15:17:23 +0330

    add user and address

---
+++ src/main/java/ir/maktabsharif115/jpa/domain/Address.java
@@ -0,0 +1,30 @@
+package ir.maktabsharif115.jpa.domain;
+
+import ir.maktabsharif115.jpa.base.domain.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = Address.TABLE_NAME)
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address extends BaseEntity<Long> {
+
+    public static final String TABLE_NAME = "address";
+
+    public static final String ADDRESS = "address";
+    public static final String POSTAL_CODE = "postal_code";
+
+    @Column(name = ADDRESS)
+    private String address;
+
+    @Column(name = POSTAL_CODE)
+    private String postalCode;
+}
---
+++ src/main/java/ir/maktabsharif115/jpa/domain/User.java
@@ -0,0 +1,42 @@
+package ir.maktabsharif115.jpa.domain;
+
+import ir.maktabsharif115.jpa.base.domain.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = User.TABLE_NAME)
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends BaseEntity<Long> {
+
+    public static final String TABLE_NAME = "users";
+
+    public static final String FIRST_NAME = "first_name";
+    public static final String LAST_NAME = "last_name";
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
+    public static final String MOBILE_NUMBER = "mobile_number";
+
+    @Column(name = FIRST_NAME)
+    private String firstName;
+
+    @Column(name = LAST_NAME)
+    private String lastName;
+
+    @Column(name = USERNAME, nullable = false)
+    private String username;
+
+    @Column(name = PASSWORD)
+    private String password;
+
+    @Column(name = MOBILE_NUMBER)
+    private String mobileNumber;
+}
