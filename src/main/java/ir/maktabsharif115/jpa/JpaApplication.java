11650ab4ea8e1d3c1d5d9ff074782f6c1fa21070
maxixking66 <maxixking66@gamil.com>
2024-07-18 15:12:03 +0330

    add PrePersist and PreUpdate example

--- src/main/java/ir/maktabsharif115/jpa/JpaApplication.java
+++ src/main/java/ir/maktabsharif115/jpa/JpaApplication.java
@@ -3,20 +3,19 @@ package ir.maktabsharif115.jpa;
 import ir.maktabsharif115.jpa.domain.Province;
 import ir.maktabsharif115.jpa.util.ApplicationContext;
 import jakarta.persistence.EntityManager;
+import lombok.SneakyThrows;
 
 public class JpaApplication {
 
+    @SneakyThrows
     public static void main(String[] args) {
         ApplicationContext applicationContext = ApplicationContext.getInstance();
         EntityManager entityManager = applicationContext.getEntityManager();
 
         entityManager.getTransaction().begin();
 
-        Province province = entityManager.find(Province.class, 152L);
-        entityManager.detach(province);
-        entityManager.persist(province);
-//        province = entityManager.merge(province);
-//        province.setName(province.getName().concat(province.getName()));
+        Province province = entityManager.find(Province.class, 302L);
+        province.setName(province.getName() + " #");
 
         entityManager.getTransaction().commit();
 
--- src/main/java/ir/maktabsharif115/jpa/base/domain/BaseEntity.java
+++ src/main/java/ir/maktabsharif115/jpa/base/domain/BaseEntity.java
@@ -1,9 +1,6 @@
 package ir.maktabsharif115.jpa.base.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -32,4 +29,16 @@ public class BaseEntity<ID extends Serializable> implements Serializable {
 
     @Column(name = LAST_UPDATE_DATE)
     private ZonedDateTime lastUpdateDate;
+
+    @PrePersist
+    public void prePersist() {
+        setCreateDate(ZonedDateTime.now());
+        setLastUpdateDate(ZonedDateTime.now());
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        setLastUpdateDate(ZonedDateTime.now());
+    }
+
 }
