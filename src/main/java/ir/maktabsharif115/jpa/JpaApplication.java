58bb4e920253be1032a818be0a280a34c0f3c7dd
maxixking66 <maxixking66@gamil.com>
2024-09-05 11:45:51 +0330

    read data from excel file

--- src/main/java/ir/maktabsharif115/jpa/JpaApplication.java
+++ src/main/java/ir/maktabsharif115/jpa/JpaApplication.java
@@ -1,15 +1,13 @@
 package ir.maktabsharif115.jpa;
 
 import ir.maktabsharif115.jpa.domain.Customer;
-import ir.maktabsharif115.jpa.dto.CustomerSearch;
-import ir.maktabsharif115.jpa.repository.impl.CustomerRepositoryImpl;
-import ir.maktabsharif115.jpa.util.ApplicationContext;
-import jakarta.persistence.EntityManager;
 import lombok.SneakyThrows;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.FileOutputStream;
+import java.io.File;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 public class JpaApplication {
@@ -17,74 +15,51 @@ public class JpaApplication {
     @SneakyThrows
     public static void main(String[] args) {
 
-        EntityManager entityManager = ApplicationContext.getInstance().getEntityManager();
-        CustomerRepositoryImpl customerRepository = new CustomerRepositoryImpl(entityManager);
-        List<Customer> customers = customerRepository.findAll(
-                CustomerSearch.builder()
-                        .firstName("m")
-                        .build()
-        );
-//        TODO insert customers in excel file
-//        id  firstName  lastName  mobileNumber username
-        try (Workbook workbook = new XSSFWorkbook()) {
-            CellStyle headerStyle = getHeaderStyle(workbook);
-            Sheet sheet = workbook.createSheet();
-            insertHeaderData(
-                    sheet,
-                    new String[]{
-                            "id",
-                            "first_name",
-                            "last_name",
-                            "mobile",
-                            "username"
-                    },
-                    headerStyle
-            );
-            insertData(sheet, customers);
-            for (int i = 0; i <= 4; i++) {
-                sheet.autoSizeColumn(i);
+        try (Workbook workbook = new XSSFWorkbook(new File("customers.xlsx"))) {
+            Sheet sheet = workbook.getSheetAt(0);
+            int lastRowNum = sheet.getLastRowNum();
+            if (lastRowNum >= 1) {
+                List<Customer> customers = new ArrayList<>();
+                for (int rowNumber = 0; rowNumber <= lastRowNum; rowNumber++) {
+                    Row row = sheet.getRow(rowNumber);
+                    short lastCellNum = row.getLastCellNum();
+                    if (lastCellNum >= 1) {
+//                        StringBuilder currentRow = new StringBuilder();
+//                        for (int cellNumber = 0; cellNumber < lastCellNum; cellNumber++) {
+//                            currentRow.append(getCellStringValue(row.getCell(cellNumber)))
+//                                    .append(" --- ");
+//                        }
+//                        System.out.println(currentRow);
+                        Customer customer = new Customer();
+                        customer.setId(
+                                Long.getLong(getCellStringValue(row.getCell(0)))
+                        );
+                        customer.setFirstName(getCellStringValue(row.getCell(1)));
+                        customer.setLastName(getCellStringValue(row.getCell(2)));
+                        customer.setMobileNumber(getCellStringValue(row.getCell(3)));
+                        customer.setUsername(getCellStringValue(row.getCell(4)));
+                        customers.add(customer);
+                    }
+                }
+                customers.forEach(
+                        c -> System.out.println(c.getId() + " - " + c.getFirstName())
+                );
             }
-            try (FileOutputStream fileOutputStream = new FileOutputStream("customers.xlsx")) {
-                workbook.write(fileOutputStream);
-            }
-        }
-
-    }
-
-    private static void insertData(Sheet sheet, List<Customer> customers) {
-        int rowNum = 1;
-        for (Customer customer : customers) {
-            Row row = sheet.createRow(rowNum++);
-            row.createCell(0).setCellValue(customer.getId());
-            row.createCell(1).setCellValue(customer.getFirstName() != null ? customer.getFirstName() : "---");
-            row.createCell(2).setCellValue(customer.getLastName() != null ? customer.getLastName() : "---");
-            row.createCell(3).setCellValue(customer.getMobileNumber() != null ? customer.getMobileNumber() : "---");
-            row.createCell(4).setCellValue(customer.getUsername() != null ? customer.getUsername() : "---");
         }
     }
 
-    private static void insertHeaderData(Sheet sheet, String[] headerColumns, CellStyle headerStyle) {
-        Row row = sheet.createRow(0);
-        int cellNum = 0;
-        for (String headerColumn : headerColumns) {
-            Cell cell = row.createCell(cellNum++);
-            cell.setCellStyle(headerStyle);
-            cell.setCellValue(headerColumn);
+    public static String getCellStringValue(Cell cell) {
+        if (cell == null) {
+            return null;
         }
+        if (CellType.STRING.equals(cell.getCellType()) || CellType.BLANK.equals(cell.getCellType())) {
+            return cell.getStringCellValue().trim();
+        } else if (CellType.NUMERIC.equals(cell.getCellType())) {
+            return new DecimalFormat("###").format(cell.getNumericCellValue()).trim();
+        } else if (CellType.FORMULA.equals(cell.getCellType())) {
+            return cell.getCellFormula().trim();
+        }
+        throw new RuntimeException("wrong cell value");
     }
 
-    private static CellStyle getHeaderStyle(Workbook workbook) {
-        Font headerFont = workbook.createFont();
-        headerFont.setBold(true);
-        headerFont.setFontHeightInPoints((short) 14);
-        headerFont.setColor(IndexedColors.WHITE.getIndex());
-        // Create a CellStyle with the font
-        CellStyle headerCellStyle = workbook.createCellStyle();
-        headerCellStyle.setAlignment(HorizontalAlignment.CENTER);
-        headerCellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
-        headerCellStyle.setFont(headerFont);
-        headerCellStyle.setFillForegroundColor(IndexedColors.RED.getIndex());
-        headerCellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-        return headerCellStyle;
-    }
 }
