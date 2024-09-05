5615e4c26d399f8e90f7d51514c4b835942a3267
maxixking66 <maxixking66@gamil.com>
2024-09-05 09:35:42 +0330

    write string in file 1

--- src/main/java/ir/maktabsharif115/jpa/JpaApplication.java
+++ src/main/java/ir/maktabsharif115/jpa/JpaApplication.java
@@ -1,34 +1,23 @@
 package ir.maktabsharif115.jpa;
 
-import com.github.javafaker.Faker;
-import ir.maktabsharif115.jpa.domain.User;
-import ir.maktabsharif115.jpa.util.ApplicationContext;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.TypedQuery;
 import lombok.SneakyThrows;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 
 public class JpaApplication {
 
-    static Faker faker = new Faker();
-
-
     @SneakyThrows
     public static void main(String[] args) {
-        ApplicationContext applicationContext = ApplicationContext.getInstance();
-        EntityManager entityManager = applicationContext.getEntityManager();
+        String name = "\nAli alavi\n";
 
-        TypedQuery<User> typedQuery = entityManager.createQuery("from User u ", User.class);
+//        File file = new File();
 
-//        EntityGraph<?> test = entityManager.createEntityGraph("test");
-//        test.addAttributeNodes("roles", "roles.operations");
-
-        typedQuery
-                .setHint("jakarta.persistence.fetchgraph",
-                        entityManager.getEntityGraph(User.FULL_GRAPH)
-                ).setMaxResults(5);
-
-        System.out.println(typedQuery.getResultList().size());
-
+        BufferedWriter bufferedWriter = new BufferedWriter(
+                new FileWriter("my-name.txt")
+        );
+        bufferedWriter.write(name);
+        bufferedWriter.close();
     }
 
 }
