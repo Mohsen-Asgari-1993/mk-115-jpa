4d8d657db0961f6609fac311d3e3cac4bd7388f7
maxixking66 <maxixking66@gamil.com>
2024-09-05 09:50:46 +0330

    read string from file 1

--- src/main/java/ir/maktabsharif115/jpa/JpaApplication.java
+++ src/main/java/ir/maktabsharif115/jpa/JpaApplication.java
@@ -2,22 +2,33 @@ package ir.maktabsharif115.jpa;
 
 import lombok.SneakyThrows;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 public class JpaApplication {
 
     @SneakyThrows
     public static void main(String[] args) {
-        String name = "\nAli alavi\n";
 
-//        File file = new File();
+        StringBuilder resultStringBuilder = new StringBuilder();
+        try (
+                BufferedReader br = new BufferedReader(
+                        new InputStreamReader(
+                                new FileInputStream("my-name.txt")
+                        )
+//                        new FileReader(
+//                                "my-name.txt"
+//                        )
+                )
+        ) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                resultStringBuilder.append(line).append("\n");
+            }
+        }
 
-        BufferedWriter bufferedWriter = new BufferedWriter(
-                new FileWriter("my-name.txt")
-        );
-        bufferedWriter.write(name);
-        bufferedWriter.close();
+        System.out.println(resultStringBuilder);
     }
 
 }
